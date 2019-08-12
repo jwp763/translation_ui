ea027c72a7e8ffd0491de6f37fa1a28735a3efe0
jwp763 <johnpope@John-Popes-MacBook-Pro.local>
2019-08-12 13:47:44 -0700

    refactored code a bit

--- load_data.js
+++ load_data.js
@@ -1,35 +1,32 @@
 $.getJSON( "translation_text.json", function( data ) {
   var translations = [];
   $.each( data.translations, function( key, val ) {
-    var source_list = this.sourceText.split(" ");
-    var source_str = "";
-    var source_tags = this.sourceTags
-    source_list.forEach(function(item, index){
 
-      if(source_tags.includes(index)){
-        source_str += "<mark>" + item + "</mark>" + " " ;
-      } else {
-        source_str += item + " ";
-      }
-    });
+    function formatErrors(text, tags) {
+      var text_list = text.split(" ");
+      var output_str = "";
+      text_list.forEach(function(item, index){
 
-    var translated_list = this.translatedText.split(" ");
-    var translated_str = "";
-    var translated_tags = this.tags
-    translated_list.forEach(function(item, index){
+        if(tags.includes(index)){
+          output_str += "<mark>" + item + "</mark>" + " " ;
+        } else {
+          output_str += item + " ";
+        }
+      });
 
-      if(translated_tags.includes(index)){
-        translated_str += "<mark>" + item + "</mark>" + " " ;
-      } else {
-        translated_str += item + " ";
-      }
-    });
+      return output_str;   // The function returns the product of p1 and p2
+    }
+    translated_str = formatErrors(this.translatedText, this.tags);
+    source_str = formatErrors(this.sourceText, this.sourceTags)
 
-
-    translations.push( '<div class="row"><div>'+this.lineNumber+'</div><div class="six wide column"><div class="ui segment">')
-    translations.push(source_str+'</div></div><div class="six wide column"><div class="ui segment">'+translated_str)
-    translations.push('</div></div><div class="three wide column"><div class="ui segment" style="-webkit-box-shadow: none;-moz-box-shadow: none;box-shadow: none; border: none"><div class="ui progress tiny teal"  data-percent="0" id="example">')
-    translations.push('<div class="bar" style="width: '+(100-100*this.hter.toFixed(2))+'%"></div><div class="label">'+(100-100*this.hter.toFixed(2))+'/100</div></div></div></div></div>' );
+    var quality_score = 100-100*this.hter.toFixed(2);
+    translations.push( '<div class="row"><div>'+this.lineNumber);
+    translations.push('</div><div class="six wide column"><div class="ui segment">'+source_str);
+    translations.push('</div></div><div class="six wide column"><div class="ui segment">'+translated_str);
+    translations.push('</div></div><div class="three wide column">');
+    translations.push('<div class="ui segment borderless" ><div class="ui progress tiny teal">');
+    translations.push('<div class="bar" style="width: '+quality_score+'%"></div>' );
+    translations.push('<div class="label">'+quality_score+'/100</div></div></div></div></div>');
   });
  
   $( "<div/>", {
@@ -38,6 +35,5 @@ $.getJSON( "translation_text.json", function( data ) {
   }).appendTo( "#show-data" );
 });
 
-$('#example').progress();
 
 
