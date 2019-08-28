f0179475554f4d5c169035d5e12a4d3f5c301463
jwp763 <johnpope@John-Popes-MacBook-Pro.local>
2019-08-28 11:38:07 -0700

    Added the parrot joke

--- js/load_data.js
+++ js/load_data.js
@@ -1,6 +1,7 @@
 function setup(){
   $.getJSON( "resources/translation_text"+$('.ui.dropdown').dropdown('get value')+".json", function( data ) {
     var translations = [];
+    var correctionTerms = ["substitution", "insertion", "deletion", "shift"];
     $.each( data.translations, function( key, val ) {
 
       translated_str = this.translatedText;
@@ -9,8 +10,7 @@ function setup(){
 
 
       var quality_score = 100-parseInt(100*this.hter.toFixed(2));
-      var correctionTerms = [" substitution", " insertion", " deletion", " shift"];
-      var correctionColors = ["purple", " green", " orange", " blue"];
+      var correctionColors = ['"color:red;"', '"color:green;"', '"color:red; text-decoration: line-through"', '"font-weight:bold;"'];
       translations.push('<div class="row"><div>'+this.lineNumber+'</div>');
       translations.push('<div class="four wide column"><div class="ui segment"><p class="sourceText'+key+'">'+source_str+'</p></div></div>');
       translations.push('<div class="four wide column"><div class="ui segment"><p class="translatedText'+key+'">'+translated_str+'</p></div></div>');
@@ -21,21 +21,20 @@ function setup(){
       var i;
       for (i = 0; i < 4; i++) { 
         if (this.numCorrections[i] > 0 ){
-          translations.push('<div class="godMode hter" style="color:'+ correctionColors[i]+'">'+ this.numCorrections[i] + correctionTerms[i]);
+          translations.push('<div class="godMode hter '+ correctionTerms[i] + key +'">' + this.numCorrections[i]);
+          if ((i == 1) || (i==3)){
+            translations.push('<span style="color:red;"> ^</span>');
+          }
+          translations.push('<span style=' + correctionColors[i] + '> ' + correctionTerms[i]  );
           if (this.numCorrections[i] > 1){
             translations.push('s');
           };
-          translations.push('</div>');
+          translations.push('</span></div>');
         };  
       };
+
       translations.push('</div></div>');
     });
-
-
-
-
-
-
 
 
     $("#show-data").empty()
@@ -60,14 +59,141 @@ function setup(){
       styleFromTags(this.corrShiftTags, key, 'p.correctedText', 'font-weight', 'bold');
       styleFromTags(this.tags, key, 'p.translatedText', 'color', 'red');
       styleFromTags(this.correctionTags, key, 'p.correctedText', 'font-weight', 'bold');
-      styleFromTags(this.corrInsTags, key, 'p.correctedText', 'font-weight', 'bold');
+      styleFromTags(this.corrInsTags, key, 'p.correctedText', 'color', 'green');
       styleFromTags(this.transInsTags, key, 'p.translatedText', 'color', 'red');
 
       if ($('input[id=godToggle]').prop('checked')){
         styleFromTags(this.transDelTags, key, 'p.translatedText', 'text-decoration', 'line-through');
       }else{
         styleFromTags(this.transDelTags, key, 'p.translatedText', 'text-decoration', 'none');
-      }
+      } 
+
+
+      var transTagArrays = [this.tags,this.transInsTags,this.transDelTags,this.transShiftTags];
+      var corrTagArrays = [this.correctionTags,this.corrInsTags,[],this.corrShiftTags];
+
+      var l = [0,1,2,3];
+
+      numCorrs = this.numCorrections;
+
+      $.each(l, function (index, i) { 
+
+        if (numCorrs[i] > 0 ){
+
+          var correctionTerm = correctionTerms[i];
+          var refString = 'div.'+correctionTerm+key;
+          var transTag = transTagArrays[i];
+          var corrTag = corrTagArrays[i];
+          $(refString).hover(function() { 
+                            $(refString).css('font-weight', 'bold');
+
+                              }, function() { 
+                                $(refString).css('font-weight', 'normal');
+
+                              }); 
+
+          $.each(transTag, function (index, wordNum) {
+
+            $(refString).hover(function() { 
+                            $('p.translatedText' + key + ' span.word' + wordNum).css('background-color', 'yellow');
+                              }, function() { 
+                                $('p.translatedText' + key + ' span.word' + wordNum).css('background-color', 'white');
+                              }); 
+          })
+
+          $.each(corrTag, function (index, wordNum) {
+
+            $(refString).hover(function() { 
+                            $('p.correctedText' + key + ' span.word' + wordNum).css('background-color', 'yellow');
+                              }, function() { 
+                                $('p.correctedText' + key + ' span.word' + wordNum).css('background-color', 'white');
+                              }); 
+          })
+
+
+
+
+
+
+        };  
+      });
+
+
+/*
+
+      $('div.substitution'+key).hover(function() { 
+                        $('div.substitution'+key).css('font-weight', 'bold');
+
+                          }, function() { 
+                            $('div.substitution'+key).css('font-weight', 'normal');
+
+                          }); 
+
+      $.each(this.tags, function (index, wordNum) {
+
+        $('div.substitution'+key).hover(function() { 
+                        $('p.translatedText' + key + ' span.word' + wordNum).css('background-color', 'yellow');
+                          }, function() { 
+                            $('p.translatedText' + key + ' span.word' + wordNum).css('background-color', 'white');
+                          }); 
+      })
+
+      $.each(this.correctionTags, function (index, wordNum) {
+
+        $('div.substitution'+key).hover(function() { 
+                        $('p.correctedText' + key + ' span.word' + wordNum).css('background-color', 'yellow');
+                          }, function() { 
+                            $('p.correctedText' + key + ' span.word' + wordNum).css('background-color', 'white');
+                          }); 
+      })
+
+
+
+
+
+
+
+
+
+
+
+
+      $('div.insertion'+key).hover(function() { 
+                        $('div.insertion'+key).css('font-weight', 'bold');
+
+                          }, function() { 
+                            $('div.insertion'+key).css('font-weight', 'normal');
+
+                          }); 
+
+      $.each(this.transInsTags, function (index, wordNum) {
+
+        $('div.insertion'+key).hover(function() { 
+                        $('p.translatedText' + key + ' span.word' + wordNum).css('background-color', 'yellow');
+                          }, function() { 
+                            $('p.translatedText' + key + ' span.word' + wordNum).css('background-color', 'white');
+                          }); 
+      })
+
+      $.each(this.corrInsTags, function (index, wordNum) {
+
+        $('div.insertion'+key).hover(function() { 
+                        $('p.correctedText' + key + ' span.word' + wordNum).css('background-color', 'yellow');
+                          }, function() { 
+                            $('p.correctedText' + key + ' span.word' + wordNum).css('background-color', 'white');
+                          }); 
+      })
+
+
+
+
+
+*/
+
+
+
+
+
 
       var translationInsertionTags = this.transInsTags;
       $.each(this.mappings, function( index, mapTriplet ) {
@@ -85,11 +211,11 @@ function setup(){
                     ( !((classStrB == "translated") && translationInsertionTags.includes(sourceNum)) &&
                       !((classStrB == "source") && translationInsertionTags.includes(selfNum)))
                     ){
-                  $('p.'+classStrB+'Text' + key + ' span.word' + sourceNum).hover(function() { 
-                    $('p.'+classStrA+'Text' + key + ' span.word' + selfNum).css('background-color', 'yellow');
-                      }, function() { 
-                        $('p.'+classStrA+'Text' + key + ' span.word' + selfNum).css('background-color', 'white');
-                      }); 
+                      $('p.'+classStrB+'Text' + key + ' span.word' + sourceNum).hover(function() { 
+                        $('p.'+classStrA+'Text' + key + ' span.word' + selfNum).css('background-color', 'yellow');
+                          }, function() { 
+                            $('p.'+classStrA+'Text' + key + ' span.word' + selfNum).css('background-color', 'white');
+                          }); 
                 };
               });
             });
@@ -97,15 +223,15 @@ function setup(){
         });
       });
 
-          var hidden_elements = document.getElementsByClassName("godMode");
+      var hidden_elements = document.getElementsByClassName("godMode");
 
-    for (var i = 0; i < hidden_elements.length; i++){
-      if ($('input[id=godToggle]').prop('checked')){
-        hidden_elements[i].style.display = "block";
-      }else{
-        hidden_elements[i].style.display = "none";
+      for (var i = 0; i < hidden_elements.length; i++){
+        if ($('input[id=godToggle]').prop('checked')){
+          hidden_elements[i].style.display = "block";
+        }else{
+          hidden_elements[i].style.display = "none";
+        };
       };
-    };
       
       $('.ui.dropdown').dropdown({
                       onChange: function (val) {
@@ -113,18 +239,12 @@ function setup(){
                         }
       });
 
-
       $(document).click(function(){
         $('.ui.dropdown').blur();
       });
-
-      /* Clicks within the dropdown won't make
-         it past the dropdown itself */
       $('.ui.dropdown').click(function(e){
         e.stopPropagation();
       });
-
-
     });
   });
 };
@@ -143,11 +263,6 @@ function styleFromTags(tags, rowNum, columnString, styleType, styleValue) {
 
 
 
-function toggle(className, obj) {
-  setup();
-    //$(className).toggle( obj.checked )
-
-};
 
 
 
